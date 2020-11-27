d643ff316a91c0e37851aa48ea1df775491333a9
Shehzad <shaxad.here@gmail.com>
2020-11-27 17:59:35 +0500

    edit customer

--- Controllers/Admin/CustomerEdit.php
+++ Controllers/Admin/CustomerEdit.php
@@ -12,11 +12,11 @@ if(isset($_GET['uuid'])){
     $customer = mysqli_fetch_array($fetched);
 }
 ?>
-<div class="modal fade" id="viewDetails" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="editCustomer" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog" role="document">
         <div class="modal-content tx-14">
           <div class="modal-header">
-            <h6 class="modal-title" id="exampleModalLabel">Customer Details</h6>
+            <h6 class="modal-title" id="exampleModalLabel">Edit Customer Details</h6>
             <button type="button" class="close" data-dismiss="modal" onclick="window.location.reload()" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
@@ -26,7 +26,7 @@ if(isset($_GET['uuid'])){
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary tx-13" data-dismiss="modal" onclick="window.location.reload()">Close</button>
-            <button type="button" id="btnEdit" class="btn btn-primary tx-13 editCustomer">Edit</button>
+            <button type="button" class="btn btn-primary tx-13">Edit</button>
           </div>
         </div>
       </div>
@@ -34,25 +34,6 @@ if(isset($_GET['uuid'])){
 
     <script>
     $(document).ready(function() {
-        $('#viewDetails').modal('show');
-    });
-
-    $(document).ready(function () {
-      $("#btnEdit").click(function () {
-        $('#viewDetails').modal('hide');
-        // Get he content from the input box
-        var uuid = this.value;
-        $.ajax({
-          type: "POST",
-          url: "CustomerEdit?uuid="+uuid,
-          success: function (response) {
-            $('#modelForm').empty();
-            $('#modelForm').append(response);
-          },
-          error: function (e) {
-            alert(e); 
-          }
-        })
-      });
+        $('#editCustomer').modal('show');
     });
     </script>
